a9ff7b7adb4e86194086a0f3ec7b2ea0295ac754
Eugene <joker3lolz@gmail.com>
2023-12-18 15:34:38 +0300

    First task code. In process.

---
+++ 1st task/Program.cs
@@ -0,0 +1,30 @@
+﻿string q = "q";
+Console.WriteLine("Enter the number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+EnterNumber();
+void EnterNumber()
+{
+    bool isFind = false;
+    if ((string)num = q){
+        isFind == true;
+    };
+    int numSum = num / 10 + num % 10;
+    if(numSum % 2 != 0)
+    {
+        Console.WriteLine($"Stop!");
+    }
+    else if(isFind = true)
+    {
+        Console.WriteLine($"Stop!");
+    }
+    else
+    {
+        Console.WriteLine("Enter the number");
+        num = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+
