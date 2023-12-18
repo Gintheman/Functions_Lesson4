5869dfbc256633278c5ae4e5b1e841ee1d38650c
Eugene <joker3lolz@gmail.com>
2023-12-18 16:11:15 +0300

    3d task code. In process.

---
+++ 3d task/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine($"Enter the array length");
+int num = Convert.ToInt32(Console.ReadLine());
+ReverseArray();
+void ReverseArray() 
+{
+    int[] array = new int[num];
+    int[] array2 = new int[num];
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+        System.Console.Write(array[i] + " ");
+        
+    };
+    for(int i = 0; i < array.Length; i++)
+    {
+        array2[i] = array[num - i];
+        System.Console.Write(array2[i] + " ");
+    }
+}
+
+
+
+
