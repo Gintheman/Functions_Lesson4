849c9fb04001f6c8665326eb19c78b64e625be35
Eugene <joker3lolz@gmail.com>
2023-12-18 15:41:15 +0300

    2nd task is done.

---
+++ 2nd task/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine($"Enter the array length");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[num];
+
+CreateArray();
+int[] CreateArray () 
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+        System.Console.Write(array[i] + " ");
+    };
+
+    return array; // возвращает результат функции
+};
+
+
+FindNumber();
+void FindNumber ()
+{
+    int countNumber = 0;
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+        {
+        countNumber++;
+        }
+    };
+
+    Console.WriteLine($"\n{countNumber}");
+};
