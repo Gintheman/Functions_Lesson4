05d83366ab024c218042778bec10371765fefab3
Eugene <joker3lolz@gmail.com>
2023-12-20 13:08:39 +0300

    Final code of 1st task. No idea how to fix it.

--- 1st task/Program.cs
+++ 1st task/Program.cs
@@ -1,30 +1,119 @@
-﻿string q = "q";
-Console.WriteLine("Enter the number");
-int num = Convert.ToInt32(Console.ReadLine());
-
-
-
-EnterNumber();
-void EnterNumber()
+﻿EnterNumber();
+int[] EnterNumber()
 {
-    bool isFind = false;
-    if ((string)num = q){
-        isFind == true;
-    };
-    int numSum = num / 10 + num % 10;
-    if(numSum % 2 != 0)
+    Console.WriteLine("Enter the number");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int numSum = 0;
+    int[] array = new int[num];
+    for(int i = 0; i < array.Length; i++)
+    {
+    // if(num > 100)
+    // {
+        
+    //     array[i] = num / 100;
+    //     numSum = numSum + array[i];
+    // }
+
+    // if(num > 10)
+    // {
+    //     array[i] = num / 10;
+    //     numSum = numSum + array[i];
+    // }
+
+    if(num > 1)
+    {
+        array[i] = num % 10;
+        numSum = numSum + array[i];
+    }
+    
+
+    }
+
+    if(numSum % 2 == 0)
     {
         Console.WriteLine($"Stop!");
     }
-    else if(isFind = true)
-    {
-        Console.WriteLine($"Stop!");
-    }
-    else
-    {
-        Console.WriteLine("Enter the number");
-        num = Convert.ToInt32(Console.ReadLine());
-    }
+    return array;
 }
+
+
+
+
+
+
+
+
+
+
+// EnterNumber();
+// void EnterNumber()
+// {
+//     Console.WriteLine("Enter the number");
+//     int num = Convert.ToInt32(Console.ReadLine());
+//     if(num == "q" || num % 10 == 0)
+//     {
+//         Console.WriteLine($"Stop!");
+//     }    
+//     else 
+//     {
+//         Console.WriteLine("Enter the number");
+//         int num = Convert.ToInt32(Console.ReadLine());  
+//     }
+    
+// };
+
+
+
+
+
+
+
+
+
+
+// void FillArray()
+// {
+//     int[] array = new int[num];
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         if(num > 10)
+//         {
+//             array[i] = num % 10;
+//         }
+        
+//         if(array[i] = "q")
+//         {
+//             Console.WriteLine($"Stop!");
+//             break;
+//         }
+//         else
+//         {
+//             Console.WriteLine("Enter the number");
+//             num = Convert.ToInt32(Console.ReadLine());
+//         }
+
+//     };    
+// };
+
+
+// EnterNumber();
+// void EnterNumber()
+// {
+//     // bool isFind;
+//     // if (q = "q"){
+//     //     isFind = true;
+//     //     Console.WriteLine($"Stop!");
+//     // };
+//     int numSum = num / 10 + num % 10;
+//     if(numSum % 2 != 0)
+//     {
+//         Console.WriteLine($"Stop!");
+//     }
+//       else
+//     {
+//         Console.WriteLine("Enter the number");
+//         num = Convert.ToInt32(Console.ReadLine());
+//     }
+// }
 
 
